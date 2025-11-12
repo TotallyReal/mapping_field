2cbad95567f01d613c576d881fefe7a61bb9c100
ofirdavid <eofirdavid@gmail.com>
2025-11-12 10:42:20 +0200

    code formatter

--- mapping_field/scripts/format_code.py
+++ mapping_field/scripts/format_code.py
@@ -1,3 +1,4 @@
+import black
 import isort
 
 from mapping_field.global_config import PROJECT_ROOT
@@ -5,6 +6,18 @@ from mapping_field.global_config import PROJECT_ROOT
 src_root = PROJECT_ROOT / "mapping_field"
 ignore_dir = src_root / "old_code"
 
+# print("\n ================= Running black ================= ")
+# for py_file in src_root.rglob("*.py"):
+#     if py_file.is_relative_to(ignore_dir):
+#         continue
+#     black.format_file_in_place(
+#         py_file,
+#         fast=False,
+#         mode=black.FileMode(line_length=120),
+#         write_back=black.WriteBack.YES,
+#     )
+
+print("\n ================= Running isort ================= ")
 for py_file in src_root.rglob("*.py"):
     if py_file.is_relative_to(ignore_dir):
         continue
@@ -16,9 +29,8 @@ for py_file in src_root.rglob("*.py"):
         use_parentheses=True,
         line_length=100,
         combine_as_imports=True,
-        known_first_party=['mapping_field'],
+        known_first_party=["mapping_field"],
         lines_between_types=1,
     )
 
-print("Imports sorted successfully!")
-
+print("\n ================= Finished code format ================= ")
