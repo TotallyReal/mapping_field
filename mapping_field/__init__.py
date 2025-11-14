13f086043ba99a075ac2f3088fe7cd49dad6bc2b
ofirdavid <eofirdavid@gmail.com>
2025-11-14 22:37:03 +0200

    load files in init

--- mapping_field/__init__.py
+++ mapping_field/__init__.py
@@ -1,1 +1,9 @@
-
+import mapping_field.mapping_field
+# To define the arithmetic operators
+import mapping_field.arithmetics
+# To define the comparison operator
+import mapping_field.ranged_condition
+# To extract scalars
+import mapping_field.linear
+# To multiply by condition
+import mapping_field.conditional_function
