be3c204bf962412f89ebd30b1ae4113c5184ce58
ofirdavid <eofirdavid@gmail.com>
2025-12-02 17:06:56 +0200

    weak_ref tests

---
+++ mapping_field/utils/tests/conftest.py
@@ -0,0 +1,91 @@
+
+
+@pytest.fixture(autouse=True)
+def reset_static_variables():
+    Var._instances = {}
+    NamedFunc._instances = {}
+    simplifier_context.clear()
+
+
+@pytest.fixture(autouse=True)
+def reset_logs():
+    simplify_tree.reset()
+
+
+FULL_FORMAT = "%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+SIMPLE_FORMAT = "%(message)s"
+
+
+def log_to_file(log_format: str = FULL_FORMAT):
+
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            """
+            Fixture to capture logs of a single test into a file.
+            """
+            # Determine log folder
+            log_dir = Path(__file__).parent / "logs"
+            log_dir.mkdir(exist_ok=True)
+
+            # Build log filename: <test_file>__<test_name>.log
+            test_file = Path(func.__code__.co_filename).stem
+            test_name = func.__name__
+            log_file = log_dir / f"{test_file}__{test_name}.log"
+
+            # Create file handler
+            handler = logging.FileHandler(log_file, mode="w")
+            handler.setFormatter(logging.Formatter(log_format))
+
+            # Get root logger and attach handler
+            logger = logging.getLogger()
+            logger.addHandler(handler)
+            logger.setLevel(logging.DEBUG)  # capture DEBUG+ logs
+
+            try:
+                # Run the test
+                return func(*args, **kwargs)
+            finally:
+                # Cleanup handler
+                logger.removeHandler(handler)
+                handler.close()
+
+        return wrapper
+
+    return decorator
+
+
+def pytest_configure(config):
+    config.first_failure_logged = False
+
+
+def pytest_runtest_makereport(item: Item, call: CallInfo[Any]):
+    """Hook called when pytest makes a test report."""
+    global _first_failure_logged
+
+    # Run after failed tests
+    if call.when == "call" and (call.excinfo is not None):
+
+        # Only run after the first failed test
+        config = item.config
+        if config.first_failure_logged:
+            return
+        config.first_failure_logged = True
+
+        save_logs_to_file(item)
+
+
+def save_logs_to_file(item: Item):
+    # Determine log folder
+    log_dir = Path(__file__).parent / "logs"
+    log_dir.mkdir(exist_ok=True)
+
+    # Build log filename: <test_file>__<test_name>.log
+    test_file = item.fspath.basename[:-3]  # remove the '.py' at the end
+    test_name = item.name
+    log_file = log_dir / f"{test_file}__{test_name}.log_context"
+
+    simplify_tree.root_context.save_element(log_file)
+
+
+_DEBUG_STATE = 0
---
+++ mapping_field/utils/tests/weakref_test.py
@@ -0,0 +1,25 @@
+import random
+import uuid
+from typing import Any
+
+from mapping_field.utils.weakref import GenericWeakKeyDictionary
+
+class Key:
+    def __init__(self, key: int):
+        self.key = key
+        self._hash = uuid.uuid4().int
+
+    def __hash__(self) -> int:
+        return self._hash
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, Key) and self.key == other.key
+
+def test_keys():
+    weak_dict = GenericWeakKeyDictionary[Key, bool]()
+    for _ in range(10):
+        keys = [Key(i) for i in range(10)]
+        for _ in range(1000):
+            key = random.choice(keys)
+            weak_dict[key] = True
+            weak_dict.validate(key)
