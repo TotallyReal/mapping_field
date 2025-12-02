d4f74fdf03e2afb8910ccf23ede29e6b4694b70c
ofirdavid <eofirdavid@gmail.com>
2025-12-02 16:55:00 +0200

    some promise tests

--- mapping_field/tests/promises_test.py
+++ mapping_field/tests/promises_test.py
@@ -1,10 +1,13 @@
 import pytest
 
 from mapping_field.conditions import FalseCondition, TrueCondition
+from mapping_field.log_utils.tree_loggers import TreeLogger, blue
 from mapping_field.mapping_field import InvalidInput, simplifier_context, Var
 from mapping_field.property_engines import is_integral, is_condition
+from mapping_field.ranged_condition import in_range, IntervalRange
 from mapping_field.tests.utils import DummyMap
 
+simplify_logger = TreeLogger(__name__)
 
 def test_int_var_promise():
     x = Var("x", output_properties={is_integral: True})
@@ -31,6 +34,8 @@ def test_bool_var_promise():
 def test_integral_arithmetic():
     dummy1, dummy2 = DummyMap(1), DummyMap(2)
 
+    result = -dummy1
+    assert is_integral.compute(result, simplifier_context) is None
     result = dummy1 + dummy2
     assert is_integral.compute(result, simplifier_context) is None
     result = dummy1 - dummy2
@@ -38,12 +43,27 @@ def test_integral_arithmetic():
     result = dummy1 * dummy2
     assert is_integral.compute(result, simplifier_context) is None
 
-    simplifier_context.set_property(dummy1, is_integral, True)
-    simplifier_context.set_property(dummy2, is_integral, True)
+    simplify_logger.log(blue("Adding Integral promise"))
+    dummy1 = DummyMap(1, output_properties={is_integral: True})
+    dummy2 = DummyMap(2, output_properties={is_integral: True})
 
+    result = -dummy1
+    assert is_integral.compute(result, simplifier_context) is True
     result = dummy1 + dummy2
     assert is_integral.compute(result, simplifier_context) is True
     result = dummy1 - dummy2
     assert is_integral.compute(result, simplifier_context) is True
     result = dummy1 * dummy2
     assert is_integral.compute(result, simplifier_context) is True
+
+def test_integral_arithmetic_complex():
+    dummy = [DummyMap(i, output_properties={is_integral: True}) for i in range(5)]
+
+    result = (dummy[0] - dummy[1] + 3*dummy[2]) * (dummy[3] - dummy[4]) * dummy[0]
+    assert is_integral.compute(result, simplifier_context) is True
+
+def test_condition_implications():
+    dummy = DummyMap(1, output_properties={is_condition: True})
+
+    assert is_integral.compute(dummy, simplifier_context) is True
+    assert in_range.compute(dummy, simplifier_context) == IntervalRange[0,1]
